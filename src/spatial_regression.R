fb9956ea6dc6a257820a785e21742e055188bbc1
AurorArctic <aurora.maria.tumminello.z@gmail.com>
2022-02-28 22:21:38 +0100

    Create Rmd file with spatial regression and update the website

--- src/spatial_regression.R
+++ src/spatial_regression.R
@@ -3,6 +3,7 @@ library(tidyverse)
 library(spdep)
 library(rgdal)
 library(sf)
+library(sp)
 
 # Change working directory
 setwd("G:/Il mio Drive/2nd Year/Geospatial/TrentinoSchools/src")
@@ -45,30 +46,33 @@ for(i in 19:50){
 
 #####################################
 # Critical cut-off neighbourhood
-coords = coordinates(tn)
 # Threshold (all schools have a neighbour at at least 9.18km)
 knn1IT <- knn2nb(knearneigh(coords,k=1,longlat=T))
 all.linkedT <- max(unlist(nbdists(knn1IT, coords, longlat=T))) 
 all.linkedT
 
-dnb10 <- dnearneigh(coords, 0, 10, longlat=TRUE); dnb10
-dnb20 <- dnearneigh(coords, 0, 20, longlat=TRUE); dnb20
-dnb50 <- dnearneigh(coords, 0, 50, longlat=TRUE); dnb50
-dnb100 <- dnearneigh(coords, 0, 100, longlat=TRUE); dnb100
+coords = coordinates(tn)
+
+dnb10 <- dnearneigh(coords, 0, 2, longlat=TRUE); dnb10
+dnb20 <- dnearneigh(coords, 0, 5, longlat=TRUE); dnb20
+dnb50 <- dnearneigh(coords, 0, 10, longlat=TRUE); dnb50
+dnb100 <- dnearneigh(coords, 0, 20, longlat=TRUE); dnb100
 
 plot(tn, border="grey",xlab="",ylab="",xlim=NULL)
 title(main="d nearest neighbours, d = 10-100") 
-plot(dnb10, coords, add=TRUE, col="blue")
-plot(dnb20, coords, add=TRUE, col="red")
-plot(dnb50, coords, add=TRUE, col="yellow")
-plot(dnb100, coords, add=TRUE, col="green")
+plot(dnb10, coords, add=TRUE, col="lightblue")
+plot(dnb20, coords, add=TRUE, col="cornflowerblue")
+plot(dnb50, coords, add=TRUE, col="blue")
+plot(dnb100, coords, add=TRUE, col="black")
 
 ########################################
 # Contiguity based approach based on municipalities
-contnb_q <- poly2nb(munic, queen=T)
+coords = coordinates(tn)
+
+contnb_q <- poly2nb(tn, queen=T)
 contnb_q
-plot(munic, border="grey")
-plot(contnb_q, munic, add=TRUE)
+plot(tn, border="grey")
+plot(contnb_q, coords, add=TRUE)
 plot(knn2nb(knearneigh(coords, k = 5, longlat=T)), coords, add=TRUE, col="red")
 
 ######################################
@@ -76,6 +80,7 @@ dnb10.listw <- nb2listw(dnb10,style="W")
 dnb20.listw <- nb2listw(dnb20,style="W")
 dnb50.listw <- nb2listw(dnb50,style="W")
 dnb100.listw <- nb2listw(dnb100,style="W")
+
 
 distM <- as.matrix(dist(coords)) #distance matrix
 # Three possible weight matrices
@@ -93,13 +98,57 @@ listW1s <- mat2listw(W1s)
 listW2s <- mat2listw(W2s)
 listW3s <- mat2listw(W3s)
 
-length(dnb10.listw)
+length(dnb100.listw)
+tn$Scuole.tot[is.na(tn$Scuole.tot)] <- 0
+tn$Scuole.tot[is.na(tn$Media.stud)] <- 0
 
-stat_or_par = ifelse(schools@data$Gestione == "Statale",0,1)
-moran.test(stat_or_par, dnb10.listw, randomisation=FALSE)
+col_names = names(tn)
+
+na.zero <- function (x) {
+    x[is.na(x)] <- 0
+    return(x)
+}
+
+# MORAN TEST
+# Number of schools + Municipalities
+moran.test(na.zero(tn$Scuole.tot), dnb100.listw, randomisation=FALSE)
+moran.mc(italy$gprb, dnb379.listw, nsim=999)
 
 
-#######################
-# LG
-lm_model <- lm(gprb ~ , stud_schools)
-summary(LinearSolow) 
+moran.test(na.zero(tn$Studenti), dnb100.listw, randomisation=TRUE)
+
+# Mean number of students per school and class
+moran.test(na.zero(tn$Media.stud), dnb100.listw, randomisation=FALSE)
+moran.test(na.zero(tn$Media.st_1), dnb100.listw, randomisation=FALSE)
+
+# Population
+moran.test(na.zero(as.numeric(tn$Pop.under)), dnb100.listw, randomisation=FALSE)
+moran.test(na.zero(as.numeric(tn$Pop_mat)), dnb100.listw, randomisation=FALSE)
+moran.test(na.zero(as.numeric(tn$Pop_ele)), dnb100.listw, randomisation=FALSE)
+moran.test(na.zero(as.numeric(tn$Pop_med)), dnb100.listw, randomisation=FALSE)
+moran.test(na.zero(as.numeric(tn$Pop_sup)), dnb100.listw, randomisation=FALSE)
+moran.test(na.zero(as.numeric(tn$Popolazion)), dnb100.listw, randomisation=FALSE)
+
+moran.test(na.zero(as.numeric(tn$Pop_stud.P)), dnb100.listw, randomisation=FALSE)
+moran.test(na.zero(as.numeric(tn$Stud.Pop_s)), dnb100.listw, randomisation=FALSE)
+
+
+# LINEAR MODEL
+model = lm(Studenti ~ Pop.under + Scuole.tot, tn)
+summary(model)
+
+studres <- rstudent(model)
+resdistr <- quantile(studres, na.rm=TRUE) 
+colours <- grey((length(resdistr):2)/length(resdistr))
+plot(tn, col=colours[findInterval(studres, resdistr, all.inside=TRUE)])
+
+lm.morantest(model,dnb100.listw,resfun=rstudent)
+
+# MORAN SCATTERPLOT
+
+dnb321 <- dnearneigh(coordinates(tn), 0, 100, longlat=TRUE)
+dnb321.listw <- nb2listw(dnb321,style="W",zero.policy=F)
+mplot <- moran.plot(na.zero(tn$Studenti), listw=dnb321.listw, main="Moran scatterplot", 
+                    return_df=F)
+grid()
+
